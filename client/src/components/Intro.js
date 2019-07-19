53af44993ac7b6748c0db8119b3eb248ee2a1ab3
kennyware <kennyware34@gmail.com>
2019-07-19 14:18:39 -0500

    Changed text in About and Intro components

--- client/src/components/Intro.js
+++ client/src/components/Intro.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Intro() {
     return (
         <div>
-            <p>This app was created demonstrate my ability to create a full stack about using the MERN stack. I hope you are at least slightly impressed or at least enough to consider me for a developer position at your company.</p>
+            <p>This app was created to demonstrate my ability to create a full stack app using the MERN stack. I hope you are at least impressed enough to consider me for a developer position at your company.</p>
             <p>Click the log in link in the top right corner to get started.</p>
         </div>
     )
--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -4,7 +4,10 @@ export class About extends Component {
     render() {
         return (
             <div>
-                About
+                <h1>About</h1>
+                <p>This app was created demonstrate my ability to create a full stack about using the MERN stack.</p>
+                <p>One thing I would like to note is that, for this project I decided not use any state management like Redux in order to see how difficult it would be to created an app of this size without using a state manager. I ran into some obstacles here and there that forced me to deepen my understanding of React and state within it.</p> 
+                <p>I'd like reiterate that this project wasn't built with real-world use in mind, so I know that there are some issues on the backend and frontend.</p>
             </div>
         )
     }
