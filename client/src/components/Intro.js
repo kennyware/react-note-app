9127e11b8c2c201ef5a5de79ff88d4c3decf11a8
kennyware <kennyware34@gmail.com>
2019-07-20 00:18:00 -0500

    Made some changes to intro text and css

--- client/src/components/Intro.js
+++ client/src/components/Intro.js
@@ -4,7 +4,7 @@ export default function Intro() {
     return (
         <div>
             <p>This app was created to demonstrate my ability to create a full stack app using the MERN stack. I hope you are at least impressed enough to consider me for a developer position at your company.</p>
-            <p>Click the log in link in the top right corner to get started.</p>
+            <p>Click the sign up link in the top right to get started or use the log in link and use the following credentials to get started right away. Email: johndoe@test.com, Password: abc123</p>
         </div>
     )
 }
