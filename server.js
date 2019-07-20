da12abda4974d27d8a83b57290233195173652fc
kennyware <kennyware34@gmail.com>
2019-07-19 23:06:37 -0500

    Made changes for production build

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -22,6 +23,13 @@ app.use(cors())
 app.use('/api/notes/', require('./routes/notes'));
 app.use('/api/users/', require('./routes/users'));
 app.use('/api/auth/', require('./routes/auth'));
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+}
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}...`);
